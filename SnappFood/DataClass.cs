94e89f98d411df847131505313f1064f42ad4913
Amir Hossein Montazerolghaem <amir.montazer@gmail.com>
2023-12-27 15:56:24 +0330

    Add SnappFood Classes

---
+++ SnappFood/DataClass.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+namespace SnappFood
+{
+    public class OutPut
+    {
+        public double avarage { get; set; }
+        public List<OutPutComments> comments { get; set; }
+    }
+
+    public class OutPutComments
+    {
+        public string commentText { get; set; }
+    }
+
+    public class DataObject
+    {
+        public bool status { get; set; }
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+        public int count { get; set; }
+        public int pageSize { get; set; }
+        public List<Comment> comments { get; set; }
+        public Sort sort { get; set; }
+    }
+
+    public class Comment
+    {
+        public int commentId { get; set; }
+        public string date { get; set; }
+        public string createdDate { get; set; }
+        public string sender { get; set; }
+        public string customerId { get; set; }
+        public string commentText { get; set; }
+        public object deliveryComment { get; set; }
+        public int rating { get; set; }
+        public int rate { get; set; }
+        public string feeling { get; set; }
+        public int status { get; set; }
+        public string expeditionType { get; set; }
+        public List<Food> foods { get; set; }
+        public List<object> replies { get; set; }
+    }
+
+    public class Food
+    {
+        public string title { get; set; }
+    }
+
+    public class Param
+    {
+        public string key { get; set; }
+        public string title { get; set; }
+    }
+
+    public class Sort
+    {
+        public string method { get; set; }
+        public string key { get; set; }
+        public List<Param> @params { get; set; }
+        public string selected { get; set; }
+    }
+}
---
+++ SnappFood/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace SnappFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReportComments();
+            Console.ReadKey();
+        }
+
+        private static void ReportComments()
+        {
+            string json = "";
+            const string URL = "https://snappfood.ir/mobile/v1/restaurant/vendor-comment";
+            string urlParameters = "?lat=35.774&long=51.418&optionalClient=WEBSITE&client=WEBSITE&deviceType=WEBSITE&appVersion=8.1.1&UDID=6fb32aa5-6813-46d2-9266-1f867fa1e3af&vendorCode=0qqwd9&page=0&sortType=score&locale=fa";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                DataObject result = response.Content.ReadAsAsync<DataObject>().Result;
+                OutPut output = new OutPut();
+                output.avarage = result.data.comments.Average(x => x.rate);
+                output.comments = new List<OutPutComments>();
+                foreach (var comment in result.data.comments)
+                    output.comments.Add(new OutPutComments { commentText = comment.commentText });
+                json = JsonConvert.SerializeObject(output);
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+            client.Dispose();
+            Console.WriteLine(json);
+        }
+    }
+}
